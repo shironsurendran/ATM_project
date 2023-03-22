1a3c44cad6ac181dee86dcc6842161c83f6b0726
shironsurendran <111949506+shironsurendran@users.noreply.github.com>
2023-03-22 12:18:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Updatename from './managedform';
-// import MCA from './MCA Admission portal';
-// import Admisssion from './admissionportal';
-// import App from './useeffect';
-// import App from './multiple arguments& shopping cart'
-// import Cart1 from './shoppingcart';
-// import App from './ATM'
-// import App from './task tracker'
-// import App from './atm1'
-// import './index.css'
-// import Atm from'./refactoring app'
-// import App from "./App"
-// import App from './form_atmapp'
 import App from './ATM_activity';
-// import App from './Axios';
-// import App from './Axios_assignment';
-// import App from'./unit 6/mount & unmount individual square';
-// import App from './unit 6/XO';
-// import App from './unit 6/coding activity 6.2';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const menuitems=[1,2,3,4,5];
-// const getrandomcolor=()=>{
-//   let palet=["red","blue","green"]
-//   let color=palet[Math.floor(Math.random()*3)]
-//   return color;
-// }
 root.render(
   <React.StrictMode>
-    {/* < App menuitems={menuitems} /> */}
 <App/>
   </React.StrictMode>
 );
